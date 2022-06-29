0a9b37f7040c31ccc3cd104e4a1b637c1740c5cb
Mohith S R <srmohith@plansource.com>
2022-06-29 11:48:01 +0530

    dry code part-1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article ,only: [:show, :edit ,:update ,:destroy]
   def show
     
     #debugger 
-
-    @article=Article.find(params[:id])
   end
 
   def index
@@ -15,13 +14,13 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article=Article.find(params[:id])
+
   end
 
   def create
     # render plain: params[:article]
 
-    @article=Article.new(params.require(:article).permit(:title, :description))
+    @article=Article.new(article_params)
     if @article.save
       flash[:notice]="Article was successfully created"
       # redirect_to ({action:show})
@@ -38,8 +37,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article=Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice]="Article was successfully updated"
 
       # redirect_to action: "show", id: params[:id]
@@ -52,9 +50,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article=Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
+
+  private
+
+  def set_article
+    @article=Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
