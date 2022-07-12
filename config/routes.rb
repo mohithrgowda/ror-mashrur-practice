0109dbeaa1cfad828ea5a15733eccbbc440c1f84
Mohith S R <srmohith@plansource.com>
2022-07-12 06:54:18 +0530

    new user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "pages#hello"
   get "about" , to:"pages#about"
+  
 
   # resources :articles , only: [:show ,:index ,:new ,:create, :edit, :update, :destroy]
   resources :articles
+
+  get "signup" , to:"users#new"
+  resources :users, except: [:new]
 end
